e41ef9fa0c5bebf2fc3079b54e70ecff17137b03
Nattt2000 <natalie.zykova@seznam.cz>
2025-07-04 00:02:15 +0200

    try2

--- main.py
+++ main.py
@@ -5,125 +5,157 @@ author: Natálie Zýková
 email: natalie.zykova@seznam.cz
 """
 
-# vytvoření tajného čísla
+
+# Libraries
 
 import random as rd
 import time
 
-prvni_cifra = rd.randint(1,9)
-prvni_cifra_str = str(prvni_cifra)
 
-seznam_cifry = []
-seznam_cifry.append(prvni_cifra_str)
+# Introduction
 
-moznosti = list(range(0,10))
-moznosti.remove(prvni_cifra)
+separator = "-----------------------------------------------"
 
-cifry = rd.sample(moznosti, 3)
-
-for cifra in cifry:
-    cifra_str = str(cifra)
-    seznam_cifry.append(cifra_str)
-
-tajne = "".join(seznam_cifry)
-tajne_str = str(tajne)
+print(f"""Hi there!
+{separator}
+I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.
+{separator}
+Enter a number:
+{separator}""")
 
 
-# uvítání
+# Functions
 
-oddelovac = "-----------------------------------------------"
-print(f"Hi there! \n{oddelovac} \nI've generated a random 4 digit number for you. \nLet's play a bulls and cows game. \n{oddelovac}")
-print("Enter a number:")
-print(oddelovac)
+def generate_secret_number() -> str:
+    """
+    Generates secret number.
+    """
+    first_digit = rd.randint(1,9)
+    first_digit_str = str(first_digit)
+    list_digits = []
+    list_digits.append(first_digit_str)
+    options_digits = list(range(0,10))
+    options_digits.remove(first_digit)
+    digits = rd.sample(options_digits, 3)
+    for digit in digits:
+        digit_str = str(digit)
+        list_digits.append(digit_str)
+    secret_number_str = "".join(list_digits)
+    return secret_number_str
 
 
-# funkce
-
-def je_zdvojene(cislo):
-    vysledek = "right input"
-    for i in range(len(cislo)):
-        for j in range(len(cislo)):
-            if i != j and cislo[i] == cislo[j]:
-                vysledek = "wrong input"
-    return(vysledek)
-
-def bulls_cows(cislo1, cislo2):
-    slovnik = {"bull": 0, "cow": 0}
-    for i in range(len(cislo1)):
-        for j in range(len(cislo2)):
-            
-            if i == j and cislo1[i] == cislo2[j]:
-                slovnik["bull"] += 1
-    
-            if i != j and cislo1[i] == cislo2[j]:
-                slovnik["cow"] += 1
-
-    def jednotne_mnozne(klic1, klic2):
-        seznam_novych_klicu = []
-        for klic in slovnik:
-            if slovnik[klic] > 1 or slovnik[klic] == 0:
-                novy_klic = klic + "s"
-                seznam_novych_klicu.append(novy_klic)
-            else:
-                novy_klic = klic
-                seznam_novych_klicu.append(novy_klic)
-        vysledek = f"{slovnik[klic1]} {seznam_novych_klicu[0]}, {slovnik[klic2]} {seznam_novych_klicu[1]}"
-        return(vysledek)
-    return(jednotne_mnozne("bull", "cow"))
-    
-    
-
-# jádro hry
-
-start = time.time()
-
-hra = True
-pocet_pokusu = 0
-seznam_vyhodnoceni = []
-
-while hra:
-
-    pokus_str = input()
-    try:
-        pokus = int(pokus_str)
-    except ValueError:
-        print("The input must contain only digits.")
-        print(oddelovac)
+def test_duplication(attempt: str) -> str:
+    """
+    Controls if the digits in attempt number repeat.
+    If yes then writes "wrong input".
+    If not then writes "right input".
+    """
+    if len(attempt) == len(set(attempt)):
+        result = "right input"
     else:
-        if len(pokus_str) != 4:
-            print("The input must contain exactly 4 digits.")
-            print(oddelovac)
-        elif pokus_str[0] == "0":
-            print("The input must not start with 0")
-            print(oddelovac)
+        result = "wrong input"
+    return result
+
+
+def compare_bull_cow(secret: str, attempt: str) -> dict:
+    """
+    Gradually compares all the digits of the secret number with the digits of the attempted number.
+    If finds a match of a digit without a match of the location then writes cow.
+    If finds a match of a digit together with a match of its location then writes bull.
+    """
+    counts = {"bull": 0, "cow": 0}
+    for i in range(len(secret)):
+        for j in range(len(attempt)):
+            if i == j and secret[i] == attempt[j]:
+                counts["bull"] += 1
+            if i != j and secret[i] == attempt[j]:
+                counts["cow"] += 1
+    return counts
+
+
+def add_s(key_1: str, key_2: str, counts: dict) -> str:
+    """
+    Looks at values of given keys.
+    If these values eaqual 0 or bigger than 1, adds letter 's' behind the keys.
+    If these values eaqual 1, doesn't add anything and lets the key same.
+    Saves newly gained keys to the list.
+    Writes a result such as 'value_1 key_1, value_2 key_2'.
+    """
+    list_new_keys = []
+    for key in counts:
+        if counts[key] > 1 or counts[key] == 0:
+            new_key = key + "s"
+            list_new_keys.append(new_key)
         else:
-            if je_zdvojene(pokus_str) == "wrong input":
-                print("The digits must be unique.")
-                print(oddelovac)
+            new_key = key
+            list_new_keys.append(new_key)
+    result = f"{counts[key_1]} {list_new_keys[0]}, {counts[key_2]} {list_new_keys[1]}"
+    return result
+
+
+def run_game() -> None:
+    """
+    Main part of the program using all the functions above.
+    First avoids the wrong inputs.
+    If gets the right input, tests the attempted number.
+    Gives you feedback about your attempt in form of 'X bulls, Y cows'.
+    Testing until it's same as the secret number, then writes '4 bulls, 0 cows'
+    Writes the number of your attempts, form of your answers and total time of guessing.
+    """
+    start = time.time()
+    game = True
+    count_attempts = 0
+    list_results = []
+    secret_number_str = generate_secret_number()
+
+    while game:
+        attempt_number_str = input()
+        try:
+            int(attempt_number_str)
+        except ValueError:
+            print("The input must contain only digits.")
+            print(separator)
+        else:
+            if len(attempt_number_str) != 4:
+                print("The input must contain exactly 4 digits.")
+                print(separator)
+            elif attempt_number_str[0] == "0":
+                print("The input must not start with 0")
+                print(separator)
             else:
-                vyhodnoceni = bulls_cows(tajne_str, pokus_str)
-                if vyhodnoceni == "4 bulls, 0 cows":
-                    pocet_pokusu += 1
-                    seznam_vyhodnoceni.append(vyhodnoceni)
-                    if (pocet_pokusu) == 1:
-                        print(f"Correct, you've guessed the right number in {pocet_pokusu} guess!")
-                    else:
-                        print(f"Correct, you've guessed the right number in {pocet_pokusu} guesses!")
-                    hra = False
+                if test_duplication(attempt_number_str) == "wrong input":
+                    print("The digits must be unique.")
+                    print(separator)
                 else:
-                    print(vyhodnoceni)
-                    print(oddelovac)
-                    pocet_pokusu += 1
-                    seznam_vyhodnoceni.append(vyhodnoceni)
-                    
-end = time.time()
-tvuj_cas_sec = end - start
-tvuj_cas_min = tvuj_cas_sec // 60
-zbytek_sec = tvuj_cas_sec % 60
+                    counts = compare_bull_cow(secret_number_str, attempt_number_str)
+                    result = add_s("bull", "cow", counts)
+                    if counts["bull"] == 4 and counts["cow"] == 0:
+                        count_attempts += 1
+                        list_results.append(result)
+                        if (count_attempts) == 1:
+                            print(f"Correct, you've guessed the right number in {count_attempts} guess!")
+                        else:
+                            print(f"Correct, you've guessed the right number in {count_attempts} guesses!")
+                        game = False
+                    else:
+                        print(result)
+                        print(separator)
+                        count_attempts += 1
+                        list_results.append(result)
+   
+    end = time.time()
+    your_time_sec = end - start
+    your_time_min = your_time_sec // 60
+    residue_sec = your_time_sec % 60
+
+    print(f"That's amazing!")
+    for i in range(count_attempts):
+        print(f"game {i + 1}: {list_results[i]}")
+    print(f"Your time is {your_time_min:.0f} minutes and {residue_sec:.0f} seconds.")
+    print(separator)
 
 
-print(f"That's amazing!")
-for i in range(pocet_pokusu):
-    print(f"hra {i + 1}: {seznam_vyhodnoceni[i]}")
-print(f"Your time is {tvuj_cas_min:.0f} minutes and {zbytek_sec:.0f} seconds.")
-print(oddelovac)
+# Call the game
+
+run_game()
